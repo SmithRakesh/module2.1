9cbebeabd63624bd50586a86bb83b19dcf8cd8a7
smith <smithjacksj94@gmail.com>
2021-01-19 19:47:56 +0530

    edited getAvg problem

--- sprint3/day1/coding/ES6 - Intro.js
+++ sprint3/day1/coding/ES6 - Intro.js
@@ -1,5 +1,3 @@
-const getAvg = (mathematics,english,geography,science,accounts) => {
-    return mathematics+english+geography+science+accounts/5
-}
+const getAvg = (mathematics = 0,english = 0,geography = 0,science = 0,accounts = 0) =>  mathematics+english+geography+science+accounts/5
 
 console.log(getAvg(15,12,1,3,13))
